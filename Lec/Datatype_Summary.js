8f497318cc914d2eb1e4e8c0c48973ce9c399cb4
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2026-01-20 10:44:54 +0530

    commit

---
+++ Lec/Datatype_Summary.js
@@ -0,0 +1,32 @@
+//Symbol Example
+const anotherid=Symbol("123");
+console.log(typeof anotherid)
+
+const id=123;
+console.log(id===anotherid);
+
+//js is dynamic or static=dynamic
+//array 
+const Cricket=['Dhoni','Virat','Rohit']
+const num=[1,2,3]
+
+//object
+
+const obj={
+    fName:"js",
+    age:20
+}
+console.log(obj.age)
+
+//function,function def,
+//Activity 1:Deep studay fun
+
+function Haris (){
+
+    console.log("Haris")
+}
+Haris()
+
+
+
+
---
+++ Lec/string.js
@@ -0,0 +1,13 @@
+let str1="Haris"
+console.log(typeof str1)
+
+//string interpolation
+console.log(`Hello my name is ${str1}`)
+
+const gamecount=new String("Haris")
+
+console.log(gamecount.length)
+
+console.log(gamecount.toLowerCase)
+console.log(gamecount.charAt())
+console.log(gamecount.indexOf('i'))
