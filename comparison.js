965c2b0156ad4764720bc6f1388a67a0e58b6e14
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2026-01-20 09:58:03 +0530

    commit

--- comparison.js
+++ comparison.js
@@ -1,12 +1,12 @@
-// console.log(2>1);//true
-// console.log(2<1);//false
-// console.log(2>=1);//true
-// console.log(2<=1);//false
-// console.log(2!=1);//true
-// console.log(2==1);//false
+console.log(2>1);//true
+console.log(2<1);//false
+console.log(2>=1);//true
+console.log(2<=1);//false
+console.log(2!=1);//true
+console.log(2==1);//false
 
-// console.log("2">1);
-// console.log("0">1);
+console.log("2">1);
+console.log("0">1);
 
 
 console.log(null>0);//false
