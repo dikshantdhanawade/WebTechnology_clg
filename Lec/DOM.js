4e943fa27b963a1ea7c1410cee5f6a90b954d58e
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2026-02-03 10:40:54 +0530

    lec

---
+++ Lec/DOM.js
@@ -0,0 +1,4 @@
+//Documnet object  model:it is bridge betn html and javascript.
+//dom is programming interface .main browser load and html .it convert in object ,thaat object structure is called dom.
+//js uses dom to read change add delete changes html.
+
