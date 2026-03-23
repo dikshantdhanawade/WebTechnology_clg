d529377a2836671daa7d85c378b376d940bb461d
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2026-03-23 20:26:47 +0530

    commit

--- lab/server.js
+++ lab/server.js
@@ -1,12 +1,30 @@
-let http=require('http')
-let server=http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Hello World! This is my tiny HTTP server.')
+const express = require('express');   // import express
+const app = express();                // create app
+              // create express app
+const PORT = 3000;                    // server port
+
+// Middleware
+app.use((req, res, next) => {
+    console.log("Request received from client");
+    next(); // go to next step
 });
 
-const PORT=3000;
-
-server.listen(PORT,()=>{
-    console.log('server is running on http://localhost:${PORT}');
+// Route 1
+app.get('/', (req, res) => {
+    res.send("Welcome to my first Express Server!");
 });
 
+// Route 2
+app.get('/about', (req, res) => {
+    res.send("This is About Page");
+});
+
+// Route 3
+app.get('/contact', (req, res) => {
+    res.send("Contact us at example@gmail.com");
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
